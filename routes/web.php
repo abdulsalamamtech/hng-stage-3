cb98fb00fce8ac1709e669ce5cde96a5816c1112
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-02 21:48:32 +0100

    Modified web route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 
 use App\Http\Controllers\AiAgentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +17,52 @@ Route::get('/', function () {
 // Name: Emergent Assistant Title: Emergency Tips
 // Job: A chat bot that answers medical emergency related questions & help with tips!
 Route::get('/agent', [AiAgentController::class, 'store']);
+
+
+
+
+
+
+
+
+
+Route::get('/test', function () {
+    return [
+        'message' => 'testing application',
+        'date' => now()
+    ];
+});
+
+
+
+
+Route::get('/artisan', function () {
+    Artisan::call('inspire');
+    Artisan::call('optimize:clear');
+    Artisan::call('migrate');
+    Artisan::call('db:seed');
+    Artisan::call('optimize');
+    Artisan::call('inspire');
+    return "Artisan commands executed successfully.";
+});
+
+Route::get('/fresh', function () {
+    Artisan::call('inspire');
+    Artisan::call('optimize:clear');
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+    Artisan::call('optimize');
+    Artisan::call('inspire');
+    return "Database refreshed successfully.";
+});
+
+
+Route::get('/run', function (Request $request) {
+    if (!$request->filled('query')) {
+        return "No query parameter provided.";
+    }
+    $query = $request->input('query');
+    Artisan::call($query);
+    Artisan::call('inspire');
+    return "command executed";
+});
