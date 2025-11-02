e7beafe9accecc1b2f8ea80a68c3c5bd1f7e3263
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-02 21:03:41 +0100

    Modified web route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+
+use App\Http\Controllers\AiAgentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// A chat bot that answers medical emergency related questions only, just tips on what to do based on described emergency.
+// Name: Emergent Assistant Title: Emergency Tips
+// Job: A chat bot that answers medical emergency related questions & help with tips!
+Route::get('/agent', [AiAgentController::class, 'store']);
